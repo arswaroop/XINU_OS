285418b357ad890526b62966257bd2250f60e7f7
Paritosh Prakash Morparia <pmorpari@silo.soic.indiana.edu>
2018-10-24 23:27:46 -0400

    Test case

--- shell/xsh_segmem.c
+++ shell/xsh_segmem.c
@@ -8,14 +8,14 @@ shellcmd xsh_segmem(int nargs, char *args[])
 	void* ptr = xmalloc(10) ;
 	printf("%s\n",xheap_snapshot());
 	int i;
-	void *ptrs[505] ;
+	void *ptrs[10] ;
 	//= (void*)malloc(52 * sizeof(uint32));	
-	for (i=0; i<505; i++)
+	for (i=0; i<10; i++)
 	{
-	        ptrs[i] = xmalloc(25) ;
+	        ptrs[i] = xmalloc(25*i) ;
 		//printf("%s\n",xheap_snapshot());
 	}
-	for (i=0; i< 505; i++)
+	for (i=0; i< 10; i++)
 	{	
 		xfree(ptrs[i]);
 		//printf("%s\n",xheap_snapshot());
